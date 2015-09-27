ff6efcd46e8e2fa3df00cce9e520ce09ab8c7bb8
mouadh jebali <mouadjebali@gmail.com>
2015-09-27 09:04:49 +0200

    WORKFLOWFILLINGDIRECTORY-12 : add helpfilling tables

--- src/sql/plugins/workflow/modules/fillingdirectory/plugin/create_db_workflow_fillingdirectory.sql
+++ src/sql/plugins/workflow/modules/fillingdirectory/plugin/create_db_workflow_fillingdirectory.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS tf_directory_cf;
 DROP TABLE IF EXISTS tf_directory_file;
 DROP TABLE IF EXISTS tf_directory_physical_file;
 DROP TABLE IF EXISTS tf_record_field_history;
+DROP TABLE IF EXISTS workflow_helpfilling_list_entries;
+DROP TABLE IF EXISTS workflow_helpfilling_cf;
 
 /*==============================================================*/
 /* Table structure for table tf_directory_cf					*/
@@ -56,3 +58,34 @@ CREATE TABLE tf_record_field_history (
   id_task INT DEFAULT 0 NOT NULL,
   PRIMARY KEY  (id_record_field,id_history,id_task)
  );
+ 
+
+/*==============================================================*/
+/* Table structure for table tf_helpfillig_cf					*/
+/*==============================================================*/
+
+CREATE TABLE workflow_helpfilling_cf(
+  id_task INT NOT NULL,
+  id_directory INT NOT NULL,
+  help_type VARCHAR(100) DEFAULT NULL,
+  is_active SMALLINT DEFAULT 0,
+  PRIMARY KEY  (id_task)
+  );
+/*==============================================================*/
+/* Table structure for table workflow_helpfilling_list_entries	*/
+/*==============================================================*/
+  
+  CREATE TABLE workflow_helpfilling_list_entries(
+  id_list_entries INT NOT NULL,
+  id_task_entry INT DEFAULT NULL,
+  id_title_parent_entry INT DEFAULT NULL,
+  id_entry INT DEFAULT NULL,
+  state  SMALLINT DEFAULT 0,
+  state_tmp SMALLINT DEFAULT 0,
+  id_directory INT NOT NULL,
+  PRIMARY KEY  (id_task_entry,id_title_parent_entry,id_entry,id_directory),
+  FOREIGN KEY (id_task_entry) REFERENCES workflow_helpfilling_cf (id_task)
+  );
+  
+
+
